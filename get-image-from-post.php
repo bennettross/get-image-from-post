ac1cfe7136600c60e929fe35d438fea3652da938
Christopher Ross <christopher.ross@rodonic.com>
2012-05-11 12:30:32 -0300

    initial commit

---
+++ get-image-from-post.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: Get Image from Post 
+Plugin URI: http://thisismyurl.com/software/web-based/wordpress-plugins/get-image-from-posts/
+Description: Used to fetch images from your posts to display as part of the excerpt.
+Author: Christopher Ross
+Tags: future, upcoming posts, upcoming post, upcoming, draft, Post, scheduled, preview
+Author URI: http://thisismyurl.com
+Version: 2.0.0
+*/
+
+/*  Copyright 2011  Christopher Ross  (email : info@christopherross.ca)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+function thisismyurl_get_image_from_post($options='') {
+    global $post, $wpdb;
+	
+	$ns_options = array(
+                    "image" => "1",
+					"show" => false,
+					"link" => false,
+					"width" => false,
+					"height" => false,
+					"strip" => false,
+                   );
+
+	$options = explode("&",$options);
+	
+	foreach ($options as $option) {
+		$parts = explode("=",$option);
+		if ($parts[1]) {$ns_options[$parts[0]] = $parts[1];}
+	}
+	
+	global $more;
+	$more = 1;
+	$content = strtolower(get_the_content());
+	$content = explode("<img",$content);
+	
+	if ($content) {
+		foreach ($content as $image) {
+			$newimage = explode(">",$image);
+			$thisimage[] = 	"<img ".$newimage[0]."/>";
+		}
+
+		$more = 0;
+		$link = get_permalink();
+		
+		if ($strip) {$thisimage[$ns_options['image']] = thisismyurl_strip_tags_attributes($thisimage[$ns_options['image']],"img","src");} 
+		if ($show) {echo $thisimage[$ns_options['image']];} else {return  $thisimage[$ns_options['image']];}
+	}
+}
+
+function thisismyurl_strip_tags_attributes($string,$allowtags=NULL,$allowattributes=NULL){
+    $string = strip_tags($string,$allowtags);
+    if (!is_null($allowattributes)) {
+        if(!is_array($allowattributes))
+            $allowattributes = explode(",",$allowattributes);
+        if(is_array($allowattributes))
+            $allowattributes = implode(")(?<!",$allowattributes); 
+        if (strlen($allowattributes) > 0)
+            $allowattributes = "(?<!".$allowattributes.")";
+        $string = preg_replace_callback("/<[^>]*>/i",create_function(
+            '$matches',
+            'return preg_replace("/ [^ =]*'.$allowattributes.'=(\"[^\"]*\"|\'[^\']*\')/i", "", $matches[0]);'    
+        ),$string);
+    }
+    return $string;
+} 
+
+?>
---
+++ readme.txt
@@ -0,0 +1,78 @@
+=== Plugin Name ===
+Contributors: christopherross
+Plugin URI: http://thisismyurl.com/downloads/wordpress/plugins/get-image-from-post/
+Tags: get,image,posts,thumbnail,preview, plugin, post, posts,images,links
+Donate link:  http://regentware.com/donate/
+Requires at least: 2.0.0
+Tested up to: 3.2
+Stable tag: 2.0.0
+
+Allows users to fetch an image from a post within the Loop.
+
+== Description ==
+
+This is a simple plugin which allows users to return an image from the related post.
+
+== Installation ==
+
+To install the plugin, please upload the folder to your plugins folder and active the plugin.
+
+== Screenshots ==
+
+== Updates ==
+Updates to the plugin will be posted here, to [thisismyurl.com](http://thisismyurl.com/)
+
+== Frequently Asked Questions ==
+
+= How do I display the results? =
+
+Insert the following code into your WordPress theme files: 
+
+= General results =
+Without passing any parameters, the plugin will return ten results or fewer depending on how many posts you have.
+
+ thisismyurl_get_image_from_post();
+
+
+= Altering the before and after values =
+By default the plugin wraps your code in list item (&lt;li&gt;) tags but you can specify how to format the results using the following code:
+
+ thisismyurl_get_image_from_post('before=&lt;p&gt;&amp;after=&lt;/p&gt;');
+
+= Adding a Link = 
+If you'd like to link to the post (remember it's not live yet) you can do so by calling:
+
+ thisismyurl_get_image_from_post('link=true'); 
+
+
+= Which image? = 
+You can specify which image is returned using the code:
+
+ thisismyurl_get_image_from_post('image=2'); 
+
+= Strip Attributes =
+If you would like to strip the attributes such as width and height from the returned value:
+
+ thisismyurl_get_image_from_post('strip=true'); 
+
+= Echo vs. Return =
+Finally, if you'd like to copy the results into a variable you can return the results as follows:
+
+ thisismyurl_get_image_from_post('show=false'); 
+
+== Donations ==
+If you would like to donate to help support future development of this tool, please visit [Regent Software](http://regentware.com/donate/?5963074)
+
+
+== Change Log ==
+
+= 2.0.0 =
+
+* renamed function to be more compatible.
+* tested and optimized in WordPress 3.2
+* added test to determine in images existed
+
+1.0.0 - official release
+	- added strip attribute
+
+1.1.0 - minor fixes for wordpress new admin
