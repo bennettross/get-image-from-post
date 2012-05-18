50685756ade268bae5b618b39bb8d3d08c9398c6
brett joe <brett.joe@rodonic.com>
2012-05-18 10:25:43 -0300

    Fixed spacing according to the WordPress coding standards.

--- get-image-from-post.php
+++ get-image-from-post.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Get Image from Post 
+Plugin Name: Get Image from Post
 Plugin URI: http://thisismyurl.com/software/web-based/wordpress-plugins/get-image-from-posts/
 Description: Used to fetch images from your posts to display as part of the excerpt.
 Author: Christopher Ross
@@ -26,60 +26,62 @@ Version: 2.0.0
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
-function thisismyurl_get_image_from_post($options='') {
+function thisismyurl_get_image_from_post( $options = '' ) {
     global $post, $wpdb;
-	
-	$ns_options = array(
-                    "image" => "1",
-					"show" => false,
-					"link" => false,
-					"width" => false,
-					"height" => false,
-					"strip" => false,
-                   );
 
-	$options = explode("&",$options);
-	
-	foreach ($options as $option) {
-		$parts = explode("=",$option);
-		if ($parts[1]) {$ns_options[$parts[0]] = $parts[1];}
+	$ns_options = array(
+	    "image" => "1",
+	    "show" => false,
+	    "link" => false,
+	    "width" => false,
+	    "height" => false,
+	    "strip" => false,
+        );
+
+	$options = explode( "&", $options );
+
+	foreach ( $options as $option ) {
+		$parts = explode( "=", $option );
+		if ( $parts[1] )
+		    $ns_options[$parts[0]] = $parts[1];
 	}
-	
+
 	global $more;
 	$more = 1;
-	$content = strtolower(get_the_content());
-	$content = explode("<img",$content);
-	
-	if ($content) {
-		foreach ($content as $image) {
-			$newimage = explode(">",$image);
+	$content = strtolower( get_the_content() );
+	$content = explode( "<img", $content ) ;
+
+	if ( $content ) {
+		foreach ( $content as $image ) {
+			$newimage = explode( ">", $image );
 			$thisimage[] = 	"<img ".$newimage[0]."/>";
 		}
 
 		$more = 0;
 		$link = get_permalink();
-		
-		if ($strip) {$thisimage[$ns_options['image']] = thisismyurl_strip_tags_attributes($thisimage[$ns_options['image']],"img","src");} 
-		if ($show) {echo $thisimage[$ns_options['image']];} else {return  $thisimage[$ns_options['image']];}
+
+		if ( $strip )
+		    $thisimage[$ns_options['image']] = thisismyurl_strip_tags_attributes( $thisimage[$ns_options['image']], "img", "src" );
+		if ( $show )
+		    echo $thisimage[$ns_options['image']];} else {return  $thisimage[$ns_options['image']];
 	}
 }
 
-function thisismyurl_strip_tags_attributes($string,$allowtags=NULL,$allowattributes=NULL){
-    $string = strip_tags($string,$allowtags);
-    if (!is_null($allowattributes)) {
-        if(!is_array($allowattributes))
-            $allowattributes = explode(",",$allowattributes);
-        if(is_array($allowattributes))
-            $allowattributes = implode(")(?<!",$allowattributes); 
-        if (strlen($allowattributes) > 0)
+function thisismyurl_strip_tags_attributes( $string, $allowtags = NULL, $allowattributes = NULL ){
+    $string = strip_tags( $string, $allowtags );
+    if ( ! is_null( $allowattributes ) ) {
+        if( ! is_array( $allowattributes ) )
+            $allowattributes = explode( ",",  $allowattributes );
+        if( is_array( $allowattributes ) )
+            $allowattributes = implode( ")(?<!", $allowattributes );
+        if ( strlen( $allowattributes ) > 0 )
             $allowattributes = "(?<!".$allowattributes.")";
-        $string = preg_replace_callback("/<[^>]*>/i",create_function(
+        $string = preg_replace_callback( "/<[^>]*>/i", create_function(
             '$matches',
-            'return preg_replace("/ [^ =]*'.$allowattributes.'=(\"[^\"]*\"|\'[^\']*\')/i", "", $matches[0]);'    
-        ),$string);
+            'return preg_replace("/ [^ =]*'.$allowattributes.'=(\"[^\"]*\"|\'[^\']*\')/i", "", $matches[0]);'
+        ), $string );
     }
     return $string;
-} 
+}
 
 ?>
